5ef50e6526293ad2756faead21f50e677d4559fe
Arya-Rabiee <arya.r1384@gmail.com>
2026-07-11 21:20:20 +0330

    add session_id to UserMessage class

--- app/models/message.py
+++ app/models/message.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
+from typing import Optional
 
 class UserMessage(BaseModel):
     message: str
-
+    session_id: Optional[str] = None
 class BotResponse(BaseModel):
     reply: str
