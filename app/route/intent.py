d88fcab829dc0bb0e7888a05515df746fa421445
Arya-Rabiee <arya.r1384@gmail.com>
2026-07-11 21:21:42 +0330

    add session to handleIntent function

--- app/route/intent.py
+++ app/route/intent.py
@@ -1,14 +1,22 @@
-from llm.model_manager import detect_intent , detect_intent_with_openai
+from llm.model_manager import detect_intent 
 from state.memory import history
 from state.state_user import conversation_state
+from llm.log import logger
 
-def handleIntent(user_message):
-    print("start")
-    recent_history = history[-6:]
-    current_city = conversation_state["current_city"]
-    intent = detect_intent(
+def handleIntent(user_message, session):
+    """
+    Intent رو detect کن
+    """
+    logger.info("handleIntent called | message: %s", user_message)
+    
+    recent_history = session.history[-6:]
+    current_city = session.conversation_state.get("current_city")
+    
+    intent_data = detect_intent(
         user_message,
         recent_history,
         current_city,
     )
-    return intent
+    
+    logger.info("Intent detected: %s", intent_data)
+    return intent_data
