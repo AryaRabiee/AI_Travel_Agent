db04d0944a36059696c389fbf2a456866324a49f
Arya-Rabiee <arya.r1384@gmail.com>
2026-07-18 16:37:31 +0330

    update recommandation_score

--- app/CBF_Recommendation/recommandation_score.py
+++ app/CBF_Recommendation/recommandation_score.py
@@ -11,34 +11,43 @@ BETA = 0.3
 def top_city(user_profile):
     logger.info("profile is %s", user_profile)
     logger.info("Start func top_city")
+    
     candidate = retrieve_top_cities(user_profile)
     logger.info("in top city candidate is %s" , candidate)
+    
     llm_score = llm_select_best_city(user_profile , candidate)
     logger.info("in top city llm_score is %s" , llm_score)
-    llm_weight = pd.Series(llm_score)
-    logger.info("in top city llm_weight is %s" , llm_weight)
-    llm_weight = llm_weight.reindex(city_df.index).fillna(0)
-    logger.info("in top city llm_weight is %s" , llm_weight)
+    
+    llm_score = {k.lower(): v for k, v in llm_score.items()}
+    logger.info("in top city llm_score (normalized) is %s" , llm_score)
+    
+    llm_weight = pd.Series(0, index=city_df.index)
+    for city, score in llm_score.items():
+        if city in city_df.index:
+            llm_weight[city] = score
+    logger.info("in top city llm_weight (expanded) is %s" , llm_weight)
+    
     algo_norm_score_llm = (llm_weight - llm_weight.min()) / (llm_weight.max() - llm_weight.min() + 1e-8)
     logger.info("in top city algo_norm_score_llm is %s" , algo_norm_score_llm)
+    
     model_score = get_weights_from_profile(user_profile)
     logger.info("in top city model_score is %s" , model_score)
-    model_weight = pd.DataFrame([model_score])
-    logger.info("in top city model_weight is %s" , model_weight)
-
-    user_weight_series = model_weight.iloc[0]
-
+    
+    user_weight_series = pd.Series(model_score)
     final_weight = user_weight_series * feaute_weight
     logger.info("in top city final_weight is %s" , final_weight)
-    score = city_df.mul(final_weight, axis=1).sum(axis=1)
-    ranked_cities = score.sort_values(ascending=False)
-    logger.info("in top city ranked_cities is %s" , ranked_cities)
-    algo_scores = ranked_cities
+    
+    algo_scores = city_df.mul(final_weight, axis=1).sum(axis=1)
+    logger.info("in top city algo_scores is %s" , algo_scores)
+    
     algo_norm = (algo_scores - algo_scores.min()) / (algo_scores.max() - algo_scores.min() + 1e-8)
+    logger.info("in top city algo_norm is %s" , algo_norm)
+    
     final_score = ALPHA * algo_norm + BETA * algo_norm_score_llm
     logger.info("in top city final_score is %s" , final_score)
+    
     top_city = final_score.idxmax()
     logger.info("top city is : %s , type top city is: %s" , top_city , type(top_city))
+    
     return {"need_more_info": False, "top_city": top_city}
-
 
